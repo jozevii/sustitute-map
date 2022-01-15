72d31970dd8f83704dcd23042d35b5b283f4c290
Josevi <josevrb98@gmail.com>
2022-01-15 04:53:32 +0100

    Add recursive calls and error catching

--- replace-pattern.js
+++ replace-pattern.js
@@ -1,29 +1,49 @@
-function replacePattern(myArray) {
-    // Preparing the returning array
-    let returnArray = myArray;
+function replacePattern(myArray, checkMode) {
+    try {
+        // Preparing the returning array
+        let returnArray = myArray;
 
-    // Iteration of the different properties in the array
-    for (nameProperty in myArray) {
-        // String of the property value
-        let stringSplit = myArray[nameProperty];
-        // Check if the value contain a clause to replace
-        if (stringSplit.includes("${") && stringSplit.includes("}")) {
-            let firstSplit = stringSplit.split("${");
-            console.log(firstSplit);
-            for (let i = 0; i < firstSplit.length; i++) {
-                if (firstSplit[i].includes("}")) {
-                    // Separation of the name of the value to replace
-                    let secondSplit = firstSplit[i].split("}");
-                    let replaceString = secondSplit[0];
-                    console.log(firstSplit[i]);
-                    console.log(replaceString);
-                    // Check if the array has this value and replace it
-                    if(typeof myArray[replaceString] !== 'undefined' && replaceString != nameProperty) returnArray[nameProperty] = returnArray[nameProperty].replace("${"+replaceString+"}",returnArray[replaceString]);     
+        // Iteration of the different properties in the array
+        for (nameProperty in myArray) {
+            // String of the property value
+            let stringSplit = myArray[nameProperty];
+            // Check if the value contain a clause to replace
+            if (stringSplit.includes("${") && stringSplit.includes("}")) {
+                let firstSplit = stringSplit.split("${");
+                for (let i = 0; i < firstSplit.length; i++) {
+                    if (firstSplit[i].includes("}")) {
+                        // Separation of the name of the value to replace
+                        let secondSplit = firstSplit[i].split("}");
+                        let replaceString = secondSplit[0];
+                        // Check if the array has this value and replace it
+                        if(typeof myArray[replaceString] !== 'undefined' && replaceString != nameProperty){
+                            if (checkMode) {
+                                // If check mode is active check if exist some clause to replace
+                                return true;
+                            } else {
+                                // If check mode is not active replace it
+                                returnArray[nameProperty] = returnArray[nameProperty].replace("${"+replaceString+"}",returnArray[replaceString]); 
+                            }
+                        } 
+                    }
                 }
             }
         }
+        if (checkMode) {
+            // If check mode is active and don't find any clause to replace return false
+            return false;
+        } else {
+            // If check mode is not active check before return the final array
+            if(replacePattern(returnArray, true)) returnArray = replacePattern(returnArray, false);
+            return returnArray;
+        }
+    } catch (e) {
+        // Catching error of Bidirectional calls
+        if (e instanceof RangeError) {
+            return "Bidirectional calls are not allowed";
+        }
     }
-    return returnArray;
+    
 }
 
 // Export of the function for testing
--- replace-pattern.test.js
+++ replace-pattern.test.js
@@ -2,136 +2,146 @@ const { expect } = require('@jest/globals');
 const replacePattern = require('./replace-pattern')
 
 test('Sample of the exercise', () => {
-    var array =  {
+    let array =  {
         "key1":"value1",
         "key2":"${key1}value2"
     };
 
-    var arrayExpected =  {
+    let arrayExpected =  {
         "key1":"value1",
         "key2":"value1value2"
     };
 
-    expect(replacePattern(array)).toStrictEqual(arrayExpected)
+    expect(replacePattern(array, false)).toStrictEqual(arrayExpected)
 });
 
 test('No changes', () => {
-    var array =  {
+    let array =  {
         "key1":"value1",
         "key2":"value2"
     };
 
-    var arrayExpected =  {
+    let arrayExpected =  {
         "key1":"value1",
         "key2":"value2"
     };
 
-    expect(replacePattern(array)).toStrictEqual(arrayExpected)
+    expect(replacePattern(array, false)).toStrictEqual(arrayExpected)
 });
 
-test('Refence to non existent property or empty propety', () => {
-    var array =  {
+test('Refence to non existent property or empty propety (I have decided to omit the auto reference)', () => {
+    let array =  {
         "key1":"value1${key3}",
         "key2":"value2${}"
     };
 
-    var arrayExpected =  {
+    let arrayExpected =  {
         "key1":"value1${key3}",
         "key2":"value2${}"
     };
 
-    expect(replacePattern(array)).toStrictEqual(arrayExpected)
+    expect(replacePattern(array, false)).toStrictEqual(arrayExpected)
 });
 
-test('Auto reference (I supose to omit the auto reference)', () => {
-    var array =  {
+test('Auto reference (I have decided to omit the auto reference)', () => {
+    let array =  {
         "key1":"value1${key1}",
         "key2":"value2"
     };
 
-    var arrayExpected =  {
+    let arrayExpected =  {
         "key1":"value1${key1}",
         "key2":"value2"
     };
 
-    expect(replacePattern(array)).toStrictEqual(arrayExpected)
+    expect(replacePattern(array, false)).toStrictEqual(arrayExpected)
 });
 
 test('Auto reference with other reference', () => {
-    var array =  {
+    let array =  {
         "key1":"value1${key1}${key2}",
         "key2":"value2"
     };
 
-    var arrayExpected =  {
+    let arrayExpected =  {
         "key1":"value1${key1}value2",
         "key2":"value2"
     };
 
-    expect(replacePattern(array)).toStrictEqual(arrayExpected)
+    expect(replacePattern(array, false)).toStrictEqual(arrayExpected)
 });
 
-test('Bidirectional refence (I supose to first get the first value)', () => {
-    var array =  {
+test('Bidirectional refence (I have decided to return a string)', () => {
+    let array =  {
         "key1":"value1${key2}",
         "key2":"value2${key1}"
     };
-
-    var arrayExpected =  {
-        "key1":"value1value2${key1}",
-        "key2":"value2value1value2${key1}"
-    };
-
-    expect(replacePattern(array)).toStrictEqual(arrayExpected)
+    expect(replacePattern(array, false)).toBe("Bidirectional calls are not allowed")
 });
 
 test('Three properties', () => {
-    var array =  {
+    let array =  {
         "key1":"value1${key2}${key3}",
         "key2":"value2",
         "key3":"value3",
     };
 
-    var arrayExpected =  {
+    let arrayExpected =  {
         "key1":"value1value2value3",
         "key2":"value2",
         "key3":"value3",
     };
 
-    expect(replacePattern(array)).toStrictEqual(arrayExpected)
+    expect(replacePattern(array, false)).toStrictEqual(arrayExpected)
+});
+
+test('Recursive references', () => {
+    let array =  {
+        "key1":"value1${key2}",
+        "key2":"value2${key3}",
+        "key3":"value3",
+    };
+
+    let arrayExpected =  {
+        "key1":"value1value2value3",
+        "key2":"value2value3",
+        "key3":"value3",
+    };
+
+    expect(replacePattern(array, false)).toStrictEqual(arrayExpected)
 });
 
 test('No $, no { and no } included', () => {
-    var array =  {
+    let array =  {
         "key1":"value1{key2}",
         "key2":"value2$key2}",
         "key3":"value3${key2",
     };
 
-    var arrayExpected =  {
+    let arrayExpected =  {
         "key1":"value1{key2}",
         "key2":"value2$key2}",
         "key3":"value3${key2",
     };
 
-    expect(replacePattern(array)).toStrictEqual(arrayExpected)
+    expect(replacePattern(array, false)).toStrictEqual(arrayExpected)
 });
 
 test('Repeated ${ or repeated }', () => {
-    var array =  {
+    let array =  {
         "key1":"${key2}}value1${${key2}",
         "key2":"value2",
     };
 
-    var arrayExpected =  {
+    let arrayExpected =  {
         "key1":"value2}value1${value2",
         "key2":"value2",
     };
 
-    expect(replacePattern(array)).toStrictEqual(arrayExpected)
+    expect(replacePattern(array, false)).toStrictEqual(arrayExpected)
 });
 
 test('Other type of values', () => {
-    expect(replacePattern(12345)).toBe(12345)
-    expect(replacePattern("aAa")).toStrictEqual("aAa")
+    expect(replacePattern(12345, false)).toBe(12345)
+    expect(replacePattern("aAa", false)).toStrictEqual("aAa")
 });
